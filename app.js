16e128540997b7a3fa54d539f3e6fd78a469bf09
wsn <l>
2020-06-07 19:15:04 +0800

    login

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express()
+app.get('/',function(req,res,next){
+    res.send('hello world!!');
+})
+app.post('/login',(req,res,next)=>{
+    
+})
+app.listen(3000,(error)=>{
+    console.log("listening on 3000")
+});
